6a0dae7b12b58ba1464575cd99155c9aef1bfcb7
foticc <foticc@163.com>
2025-02-18 11:10:57 +0800

    //compose

---
+++ src/common/access_token_request.rs
@@ -0,0 +1,66 @@
+use serde::Deserialize;
+
+struct AccessTokenRequest;
+
+impl AccessTokenRequest {
+    pub async fn request_token(token_endpoint:String,
+                               username:String,
+                               password:String,
+                               client_id:String,
+                               client_secret:String,
+                                )->Result<TokenResponse, reqwest::Error> {
+        let client = reqwest::Client::new();
+
+        let response = client
+            .post(token_endpoint)
+            .basic_auth(client_id, Some(client_secret)) // 客户端 ID 和密码
+            .form(&[
+                ("username", username),
+                ("password", password),
+                ("grant_type", "authorization_password".to_string()),
+            ])
+            .send()
+            .await?
+            .json::<TokenResponse>().await?;
+
+        log::info!("Response Status: {:?}", response);
+        Ok(response)
+    }
+
+}
+
+#[derive(Debug,Deserialize)]
+pub struct TokenResponse {
+    access_token: String,
+    refresh_token: String,
+    scope: String,
+    id_token: String,
+    token_type: String,
+    expires_in: u32,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::common::access_token_request::TokenResponse;
+
+    #[tokio::test]
+    pub async fn test_sign_in()->Result<(), reqwest::Error> {
+        let client = reqwest::Client::new();
+
+        let response = client
+            .post("http://127.0.0.1:3000/oauth2/token")
+            .basic_auth("client-msg", Some("123456")) // 客户端 ID 和密码
+            .form(&[
+                ("username", "admin"),
+                ("password", "123456"),
+                ("grant_type", "authorization_password"),
+            ])
+            .send()
+            .await?
+            .json::<TokenResponse>().await?;
+
+        println!("Response Status: {:?}", response);
+        Ok(())
+    }
+
+}
--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,3 +1,4 @@
 pub mod result;
 pub mod security;
 pub mod simple_cache;
+mod access_token_request;
